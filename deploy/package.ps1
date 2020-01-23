63a9f228fb35d1309ddc72f6eea845777c4cb0d5
Simon <homer12345@gmail.com>
2020-01-23 07:59:37 -0800

    Create separate package without stylesheets
    For people who customize their styles to ensure they don't accidentally
    overwrite them.

--- deploy/package.ps1
+++ deploy/package.ps1
@@ -32,9 +32,6 @@ Copy-Item ("x64\" + $rel + "\QHotkey.dll") -Destination $dllpath -Force
 # Copy templates
 Copy-Item .\PTA\templates\ -Destination ($pkpath + "\templates\") -Recurse -Force
 
-# Copy stylesheets
-Copy-Item .\PTA\stylesheets\ -Destination ($pkpath + "\stylesheets\") -Recurse -Force
-
 # Copy SSL lib
 if (($env:OPENSSL) -and (Test-Path $env:OPENSSL -pathType container)) {
     Copy-Item $env:OPENSSL\libcrypto-1_1-x64.dll -Destination $pkpath -Force
@@ -43,6 +40,18 @@ if (($env:OPENSSL) -and (Test-Path $env:OPENSSL -pathType container)) {
 
 # Deploy Qt
 & $windeploy $rels $binpath
+
+$pkgname = "$($pkpath)_no_stylesheets.7z"
+Write-Host "Packaging $($pkgname)..."
+
+& 7z a -t7z $pkgname .\$pkpath
+
+if ($env:APPVEYOR -eq $true) {
+    Get-ChildItem $pkgname | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+}
+
+# Copy stylesheets
+Copy-Item .\PTA\stylesheets\ -Destination ($pkpath + "\stylesheets\") -Recurse -Force
 
 $pkgname = "$($pkpath).7z"
 Write-Host "Packaging $($pkgname)..."
