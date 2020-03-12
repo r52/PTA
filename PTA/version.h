4d176b13695c20413f499cbe382f733ff6465b6c
Simon <homer12345@gmail.com>
2020-03-12 16:28:38 -0700

    Increment version

--- PTA/version.h
+++ PTA/version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define VER_FILEVERSION 0, 1, 10, 1
-#define VER_PRODUCTVERSION 0, 1, 10, 1
-#define VER_STRING "v0.1.10.1"
+#define VER_FILEVERSION 0, 2, 0, 0
+#define VER_PRODUCTVERSION 0, 2, 0, 0
+#define VER_STRING "v0.2.0"
